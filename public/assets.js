1390943655622558654b29dbdf9631c70af32c6e
lefline <issamkharbache2@gmail.com>
2024-12-25 12:33:09 +0100

    update email contact and remove TikTok link from assets

--- public/assets.js
+++ public/assets.js
@@ -3,7 +3,6 @@ import logo from "../public/issamlogo.png";
 import { FaLinkedinIn } from "react-icons/fa";
 import { FaGithub } from "react-icons/fa";
 import { IoMdMail } from "react-icons/io";
-import { FaTiktok } from "react-icons/fa6";
 
 export const assests = {
   logo,
@@ -13,7 +12,6 @@ export const assests = {
       icon: FaLinkedinIn,
     },
     { href: "https://github.com/IssamKharbache", icon: FaGithub },
-    { href: "mailto:issamkharbache2@gmail.com", icon: IoMdMail },
-    // { href: "https://www.tiktok.com/@wonder_coder", icon: FaTiktok },
+    { href: "mailto:kharbache.contact@gmail.com", icon: IoMdMail },
   ],
 };
