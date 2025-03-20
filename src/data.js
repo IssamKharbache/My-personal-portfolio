e8609ac699879176e22b4f24618ee9324e39c675
lefline <issamkharbache2@gmail.com>
2025-03-20 14:47:33 +0000

    updating the projects adding a new one

--- src/data.js
+++ src/data.js
@@ -4,20 +4,21 @@ export const itemsProject = [
     id: 1,
     title: "Shopay",
     description:
-      "Shopay is a multivendor ecommerce platform where clients can sell and buy multiple and different products.",
+      "Shopay is a scalable multi-vendor e-commerce platform where multiple sellers can manage their products, orders, and profiles. Built with Next.js, PostgreSQL, and Prisma for efficient performance and seamless user experience.",
     image: "/shopayshow.png",
     github: "https://github.com/IssamKharbache/Shopay-multivendor",
     live: "https://shopay-multivendor.vercel.app/",
   },
   {
     id: 2,
-    title: "Easy eat",
+    title: "Point Batterie Service",
     description:
-      "Easy eat is food delivery  full stack application with an user friendly admin dashboard where you can easily manage your products and also all your orders",
-    image: "/easyeatscreen.png",
-    github: "https://github.com/IssamKharbache/EasyEat",
-    live: "https://easy-eat-platform.vercel.app/",
+      "An e-commerce platform with a dashboard for easy management of online sales, inventory, and orders. It also includes a local shop system with an integrated cashier, ensuring real-time synchronization between online and in-store sales. Built with Next.js, PostgreSQL, and Prisma for high performance and scalability",
+    image: "/pointbatterie.png",
+    github: "https://github.com/IssamKharbache/point-batteries",
+    live: "https://www.pointbatteries.com/",
   },
+
   {
     id: 3,
     title: "Social up",
@@ -29,15 +30,24 @@ export const itemsProject = [
   },
   {
     id: 4,
+    title: "Easy eat",
+    description:
+      "Easy eat is food delivery  full stack application with an user friendly admin dashboard where you can easily manage your products and also all your orders",
+    image: "/easyeatscreen.png",
+    github: "https://github.com/IssamKharbache/EasyEat",
+    live: "https://easy-eat-platform.vercel.app/",
+  },
+  {
+    id: 5,
     title: "Fit Gym",
     description:
-      "Fit gym is a modern fully responsive gym website that helps the fitness fitgym company to easily reach there clients ",
+      "Modern responsive website showcasing gym deals, memberships, and promotions. Designed with Next.js for fast performance and a smooth user experience",
     image: "/fitgym.png",
     github: "https://github.com/IssamKharbache/Market-verse",
     live: "https://fit-gym-dev.vercel.app/",
   },
   {
-    id: 5,
+    id: 6,
     title: "Market verse",
     description:
       "Market verse is a market place platform where users authenticate and can buy or sell usable or new products (cars,properties,clothes...)",
@@ -46,7 +56,7 @@ export const itemsProject = [
     live: "https://market-verse.vercel.app/",
   },
   {
-    id: 6,
+    id: 7,
     title: "Royal arc",
     description:
       "Royal arc is an example of a full fledge marketing agency that specializes in branding, web design, and digital marketing.. ",
@@ -55,7 +65,7 @@ export const itemsProject = [
     live: "https://royal-arc.vercel.app/",
   },
   {
-    id: 7,
+    id: 8,
     title: "Carent",
     description:
       "Carent is a leading provider of car rental services dedicated to offering convenient and reliable transportation solutions.",
@@ -65,7 +75,7 @@ export const itemsProject = [
   },
   {
     //foodify
-    id: 8,
+    id: 9,
     title: "Foodify",
     description:
       "Foodify is a restaurent front end project built by next js and framer motion",
